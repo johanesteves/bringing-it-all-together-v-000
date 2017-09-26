6a5077a88bf691ae1541643b4118d93694c88f02
Johan Esteves <jsesteves91@gmail.com>
2017-09-26 01:03:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,14 +62,15 @@ class Dog
       self.update
     else
 
-    sql = <<-SQL
-    INSERT INTO dogs (name, breed)
-    VALUES (?, ?)
-    SQL
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
 
-    DB[:conn].execute(sql,self.name, self.breed)
+      DB[:conn].execute(sql,self.name, self.breed)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+    end
   end
 
 
