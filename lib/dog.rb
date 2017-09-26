d8459db962f41249a671cf9ea27866dc6a3af751
Johan Esteves <jsesteves91@gmail.com>
2017-09-26 01:55:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
       dog_id = dog[0]
       dog = Dog.new(id: dog[0], name: dog[1], breed: dog[2])
     else #if not found, create and save an new dog instance
-      dog self.create(name, breed)
+      dog self.create(name: name, breed: breed)
     end
 
     dog
